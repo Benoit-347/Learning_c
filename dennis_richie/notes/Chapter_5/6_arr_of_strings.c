5f2d2e80ef24c8eaf9e63f74625e1c06b6be4da5
Benoit-347 <benotk.347@gmail.com>
2025-06-02 22:09:23 +0530

    re did: pointers of pointers with arr of str

--- dennis_richie/notes/Chapter_5/6_arr_of_strings.c
+++ dennis_richie/notes/Chapter_5/6_arr_of_strings.c
@@ -2,15 +2,17 @@
 
 # include <stdio.h>
 # include <string.h>
+# include <stdlib.h>
 
 #define MAX_LINE 1000
-char arr_p[MAX_LINE];
+char *arr_p[MAX_LINE];
 
-int getline(char * str, int max_len){
+int get_line(char * str, int max_len){
     int i = 0;
     while ((*(str + i) = getchar()) != EOF && i<max_len){
         i++;
     }
+    if (i>0)
     *(str + i++) = 0;
     return i;
 }
@@ -18,20 +20,22 @@ int getline(char * str, int max_len){
 int get_text(){
     char str[MAX_LINE];
     char *str_p;
-    char *arr_ptr = arr_p;
+    char **arr_ptr = arr_p;
 
-    int n = get_line(*str, MAX_LINE);
+    int n = get_line(str, MAX_LINE);
     while (n > 0){
-        str_p = malloc(n);
+        str_p = (char *) malloc(n);
         strcpy(str_p, str);
         *arr_ptr++ = str_p;
-        int n = get_line(*str, MAX_LINE);
+    printf("Successfully read\n");
+        n = get_line(str, MAX_LINE);
     }
     *arr_ptr = 0;
+    printf("Successfully read\n");
 }
 
 int main(){
     get_text();
     for (int i = 0; arr_p[i] != 0; i++)
-        printf("string %d: %s\n", i, arr_p[i]);
+        printf("string %d: %s\n", i, (arr_p[i]));
 }
