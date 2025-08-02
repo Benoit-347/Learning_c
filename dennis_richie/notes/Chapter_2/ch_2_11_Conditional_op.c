2b8f8ece4cd909ee07a5b602f1b880d6bcb53026
Benoit-347 <benoitkuriakose@gmail.com>
2025-08-02 17:22:31 +0530

    Completed ch_3 revision

--- dennis_richie/notes/Chapter_2/ch_2_11_Conditional_op.c
+++ dennis_richie/notes/Chapter_2/ch_2_11_Conditional_op.c
@@ -8,4 +8,24 @@ int main(){
     int b = 5;
 
     printf("The output: %d\n", (a > b)? 1 : 0);
+
+
+    // precedence for same precendece in single statement is decided by compiler like:
+    printf("This is sys specific: %d %d\n", a++, a);
+    // The precedence in C is: (), [], ->, .    general immidieate ops
+                            // !, &, ++, --     Increment and address ops
+                            // *, /, %          Mathematical ops
+                            // + -
+                            // << >>            Bit shift ops
+                            // <, >, <=, >=     Cmp ops
+                            // == !=
+                            // &                Bit ops (only shift is above)
+                            // ^
+                            // |
+                            // &&               Logical  ops
+                            // ||
+                            // ?:               Conditional operator
+                            // =                Assignment operator
+                            // ,                Comma or expression only and not return operator
 }
+
---
+++ dennis_richie/notes/Chapter_3/1_.c
@@ -0,0 +1,17 @@
+// Statements in C denote its end by ';'
+// A group of statements are denoted under a say a fn, are denoted by enclosed { }. They are called compound statements or block.
+
+// a loop table cmp in C instead of linear cmps:
+    //switch
+    // syn:
+    /*
+    switch (expression){
+        case exp:
+        default:
+    }
+    */
+
+// under a looping control there is break and continue to interupt a loop
+
+// Goto 
+    // A special control only statement used to force control over to a specified label. Labels are of local memory.
