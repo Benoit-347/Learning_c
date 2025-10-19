fe181cfe4e7a2b207dc8ff73b293d01163796347
Benoit_347 <benoitkuriakose@gmail.com>
2025-10-19 15:23:24 +0530

    improved ptr fn program

--- dennis_richie/notes/Chapter_5/11_pointers_to_fns.c
+++ dennis_richie/notes/Chapter_5/11_pointers_to_fns.c
@@ -1,6 +1,7 @@
 // Want to automatically make one fn, choose what fn to use? WHile also automatically taking parameters from the parent parameters?
     // passing a function to the parameter of another function:
 
+// Program: two fns 1 and 2 return True or False, the main fn chks fn_2 is true, then returns ptr to fn_1, ptr to fn_2
 #include <stdio.h>
 
 int parent_fn(int (*comp)(int, int), int value1, int value2){   //Twostages, 1. shows the number of args for comp (int, int), 2. remaining optional parameters of parent
@@ -12,23 +13,29 @@ int parent_fn(int (*comp)(int, int), int value1, int value2){   //Twostages, 1. 
 }
 
 int fn_1(int a, int b){ // normal fn_1
-    if (a>b)
+    if (a>b){
+        printf("Reached inside fn 1: True\n");
         return 1;
-    printf("Fn 1 not true\n");
+        
+    }
+    printf("Reached inside Fn 1 not true\n");
     return 0;
 }
 
 int fn_2(int a, int b){ // normal fn_2
-    if  (!(a%10))
+    if  (!(a%10)){
+        
+        printf("Reached inside fn 2: True\n");
         return 1;
-    printf("Fn 2 not true\n");
+    }
+    printf("Reached inside Fn 2 not true\n");
     return 0;
 }
 
 int main(){
     int a = 10, b = 5;
     if ( parent_fn((fn_2(a, b)? fn_1: fn_2), a, b)) // pass either fn_1 or fn_2 to the first para of parent.
-        printf("Final: true\n");
+        printf("Reached inside Final: true\n");
     else
-        printf("Final: Not true\n");
+        printf("Reached inside Final: Not true\n");
 }
