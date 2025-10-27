5ab0d3567d1613d5314d64bb5e92e224ab453f8d
Benoit-347 <benotk.347@gmail.com>
2025-10-27 22:54:55 +0530

    repeat to last commit

---
+++ dennis_richie/notes/Chapter_6/3_struct_pointers.c
@@ -0,0 +1,4 @@
+// pointers to structs are very similar syn to var
+    // syn: struct <struct_name> *
+
+// for large structures, it is more efficient to pass pointers of a struct_obj, instead of copying all the members of the structure.
